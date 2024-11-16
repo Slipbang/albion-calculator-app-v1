09d1475cf3aea9a7b1e75bdc1f5fc6efec28f62a
Dmitry Kiselman <kiseldimon1@gmail.com>
2024-11-16 19:09:01 +0200

    added server connection (SSE)

---
+++ src/store/interface/initSSE.ts
@@ -0,0 +1,39 @@
+import {itemsHttpRequests, serverUrl} from "./interface-slice";
+import {AppDispatch} from "../index";
+
+export const initSSE = (dispatch: AppDispatch) => {
+    const jsonItems = localStorage.getItem('appConfigurationItems');
+    let appDate = '';
+
+    if (jsonItems) {
+        const items = JSON.parse(jsonItems);
+        appDate = items.date;
+    }
+
+    const eventSource = new EventSource(`${serverUrl}/date`);
+
+    eventSource.onopen = () => {
+        console.log('SSE соединение установлено');
+    };
+
+    eventSource.onmessage = async (event) => {
+        const date = await event.data;
+
+        if ((!appDate || date !== appDate) && date) {
+            dispatch(itemsHttpRequests());
+            appDate = date;
+        } else if (appDate && date && date === appDate){
+            console.log('date is valid')
+        } else if (!date) {
+            console.log('no date fetched, server error');
+        }
+
+    }
+
+    eventSource.onerror = () => {
+        console.error('Ошибка SSE-соединения');
+        //eventSource.close();
+    };
+
+    return eventSource;
+}
